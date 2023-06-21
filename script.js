3401168ee065e1e9061862644ae4cfb811c8f654
muthukumar24 <68642159+muthukumar24@users.noreply.github.com>
2023-06-21 16:39:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.write("Printing Numbers from 50 to 75 :")
+document.write("<br>")
+document.write("<br>")
+for ( let i=50 ; i<=75 ; i++)
+{
+   document.writeln(i);
+}
+document.write("<br>")
+document.write("<br>")
+document.write("Printing Numbers from 1000 to 988 :")
+document.write("<br>")
+document.write("<br>")
+for (let j=1000 ; j>=988; j--)
+{
+    document.writeln(j,"<br>");
+}
